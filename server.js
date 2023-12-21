e4562979f46def4b1c10761c38bf98398f73ce4a
Hojoon Kim <hojoon0724@gmail.com>
2023-12-20 21:15:01 -0800

    index.ejs working

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/drinks", (req, res) => {
-  res.send(drinks);
+  res.render("index.ejs");
 });
 
 // -----------------------------------------------------
